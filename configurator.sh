ba3d028fd33dc1a8ca90dd1683514934774ec85f
Pipetto-crypto <>
2022-02-15 17:52:29 +0100

    updated scripts

--- configurator.sh
+++ configurator.sh
@@ -235,11 +235,16 @@ do
 		echo -e "\nUpdated, now exiting"
 		break	
 	elif test "$opt" == "7";
+<<<<<<< HEAD
 	then	
 		if test -f temp.txt;
 		then
 			rm temp.txt
 		fi
+=======
+	then
+		rm -rf temp.txt
+>>>>>>> 3c8facf (updated scripts)
 		break
 
 	else
